c977b4f95860f679a707c0cfa19ffe68eb13a801
cmcclana <cmcclana@umich.edu>
2018-10-30 21:03:27 -0400

    working sessions with cookies

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 app = Flask(__name__)
-
-
+app.secret_key = 'CIS 4851'
 
 @app.route('/', methods=["POST", "GET"]) 
 def pii():
     if request.method == "POST":
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
+        session['first_name'] = request.form['first_name']
+        session['last_name'] = request.form['last_name']
+        return render_template('index.html', first_name=session["first_name"], last_name=session["last_name"])
+    else:
+        first_name = session['first_name'] if 'first_name' in session else ""
+        last_name = session['last_name'] if 'last_name' in session else ""
         return render_template('index.html', first_name=first_name, last_name=last_name)
-    return render_template('index.html', first_name="", last_name="")
-
